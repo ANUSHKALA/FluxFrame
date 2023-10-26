e27d87f3f29c395f825ddd8370c3d43b3fa0a9b7
Anshuman Swain <anshuman8swain@gmail.com>
2023-10-26 21:07:10 +0530

    feat:component selector + prompt space

---
+++ components/Component.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Component() {
+  const [num1, setNum1] = React.useState(0);
+  const [num2, setNum2] = React.useState(0);
+  const [sum, setSum] = React.useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSum(num1 + num2);
+  };
+
+  return (
+    <div className="bg-gray-900 w-[900px] flex justify-center items-center">
+      <div className="bg-gray-800 rounded-lg p-8 w-1/2">
+        <h1 className="text-4xl font-bold text-gray-400">Retro Machine</h1>
+        <form onSubmit={handleSubmit}>
+          <label className="block mt-4">
+            <span className="text-gray-400">Number 1:</span>
+            <input
+              type="number"
+              className="block w-full p-2 border-2 border-gray-500 rounded-md"
+              value={num1}
+              onChange={(e) => setNum1(e.target.value)}
+            />
+          </label>
+          <label className="block mt-4">
+            <span className="text-gray-400">Number 2:</span>
+            <input
+              type="number"
+              className="block w-full p-2 border-2 border-gray-500 rounded-md"
+              value={num2}
+              onChange={(e) => setNum2(e.target.value)}
+            />
+          </label>
+          <button
+            type="submit"
+            className="block w-full p-2 mt-4 bg-gray-700 rounded-md text-gray-400"
+          >
+            Calculate
+          </button>
+        </form>
+        <p className="text-2xl font-bold text-gray-400 mt-4">Sum: {sum}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
--- components/RenderScreen.jsx
+++ components/RenderScreen.jsx
@@ -1,9 +1,72 @@
+import React from "react";
+import Component from './Component'
+
 const RenderScreen = () => {
-    return(
-        <div className="bg-gray-400 h-screen">
-            <h1>Render Screen</h1>
+    const [inspectMode, setInspectMode] = React.useState(false);
+    const [selectedComponent, setSelectedComponent] = React.useState(null);
+  
+  
+    const changeInspect = () => {
+      setInspectMode(!inspectMode);
+    };
+  
+    const handleInspect = (e) => {
+      if (inspectMode) {
+        e.stopPropagation();
+        e.preventDefault();
+        const element = e.target;
+        if (selectedComponent && selectedComponent !== element) {
+          clearSelectedComponent();
+        }
+        if (element.style.border) {
+          clearSelectedComponent();
+        } else {
+          const code = element.outerHTML;
+          element.style.border = "2px solid blue";
+          element.style.position = "relative";
+          element.style.zIndex = "10";
+          element.style.background = "rgba(10,10,90,0.5)";
+          element.style.boxShadow = "0 0 10px black";
+          element.style.cursor = "pointer";
+          setSelectedComponent(element);
+          console.log(code)
+        }
+      }
+    };
+  
+    const clearSelectedComponent = () => {
+      if (selectedComponent) {
+        selectedComponent.style.border = null;
+        selectedComponent.style.position = null;
+        selectedComponent.style.zIndex = null;
+        selectedComponent.style.background = null;
+        selectedComponent.style.boxShadow = null;
+        selectedComponent.style.cursor = null;
+      }
+      setSelectedComponent(null);
+    };
+  
+    
+    return (
+      <div className="bg-gray-900 w-full h-full flex justify-center items-center">
+        <div
+          className={
+            "p-3 cursor-pointer hover:bg-slate-800 text-white fixed top-10 left-10" +
+            (inspectMode ? " bg-slate-800 border-2 border-blue-500" : " bg-slate-500")
+          }
+          onClick={changeInspect}
+        >
+          Inspect code
         </div>
-    )
+        <div
+          onClick={handleInspect}
+          
+          className="cursor-pointer"
+        >
+          <Component />
+        </div>
+      </div>
+    );
 }
 
 export default RenderScreen;
